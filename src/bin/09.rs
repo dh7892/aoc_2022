bd46cd7bf50f405c570eafa82228df4ac01c40d9
David Hills <david.hills@eigentech.com>
2022-12-09 14:24:46 +0000

    refactored 09

--- src/bin/09.rs
+++ src/bin/09.rs
@@ -1,4 +1,9 @@
 use std::collections::HashSet;
+use std::ops::{Add, Sub};
+
+use nom::character::complete::{space0, u32 as nom32};
+use nom::sequence::separated_pair;
+use nom::{character::complete::alpha1, IResult};
 
 #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
 struct Posn {
@@ -6,55 +11,98 @@ struct Posn {
     y: i32,
 }
 
-// Given head and tail positions, return new tail position
-fn move_tail(head: Posn, tail: Posn) -> Posn {
-    let (d_x, d_y) = (tail.x - head.x, tail.y - head.y);
-
-    if d_x.abs() <= 1 && d_y.abs() <= 1 {
-        // We are still close enough so no need to move tail
-        return tail.clone();
-    }
-    if d_x.abs() == d_y.abs() {
-        // We are on a diagonal so we need to stay on the diagonal
-        return Posn {
-            x: head.x + d_x / d_x.abs(),
-            y: head.y + d_y / d_y.abs(),
-        };
-    }
-    if d_x.abs() > d_y.abs() {
-        // x direction dominates so y will end up level with head
-        return Posn {
-            x: head.x + d_x / d_x.abs(),
-            y: head.y,
-        };
-    }
-    if d_x.abs() < d_y.abs() {
-        // y direction dominates so x will end up level with head
-        return Posn {
-            x: head.x,
-            y: head.y + d_y / d_y.abs(),
-        };
-    }
-
-    Posn {
-        x: tail.x,
-        y: tail.y,
+impl Default for Posn {
+    fn default() -> Self {
+        Self { x: 0, y: 0 }
     }
 }
 
+impl Posn {
+    fn signum(self) -> Self {
+        Self {
+            x: self.x.signum(),
+            y: self.y.signum(),
+        }
+    }
+    fn abs(self) -> Self {
+        Self {
+            x: self.x.abs(),
+            y: self.y.abs(),
+        }
+    }
+}
+impl Add for Posn {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+        }
+    }
+}
+impl Sub for Posn {
+    type Output = Self;
+
+    fn sub(self, other: Self) -> Self::Output {
+        Self {
+            x: self.x - other.x,
+            y: self.y - other.y,
+        }
+    }
+}
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+fn parse_direction(input: &str) -> IResult<&str, Direction> {
+    let (input, word) = alpha1(input)?;
+    match word {
+        "U" => Ok((input, Direction::Up)),
+        "D" => Ok((input, Direction::Down)),
+        "L" => Ok((input, Direction::Left)),
+        "R" => Ok((input, Direction::Right)),
+        _ => Ok((input, Direction::Right)),
+    }
+}
+
+fn parse_move(input: &str) -> IResult<&str, (Direction, u32)> {
+    separated_pair(parse_direction, space0, nom32)(input)
+}
+
+// Given head and tail positions, return new tail position
+fn move_tail(head: Posn, tail: Posn) -> Posn {
+    let diff = tail - head;
+    let unit = diff.signum();
+    let abs = diff.abs();
+
+    let mut new_pos = head + unit;
+    if abs.x > abs.y {
+        // x direction dominates, y will end up level with head
+        new_pos.y = head.y;
+    }
+    if abs.x < abs.y {
+        // y direction dominates so x will end up level with head
+        new_pos.x = head.x;
+    }
+    new_pos
+}
+
 // Apply all moves for a line and update head and tail. Track all tail locations visited
-fn execute_line(direction: &str, count: u32, rope: &mut Vec<Posn>, visited: &mut HashSet<Posn>) {
+fn execute_line(line: &str, rope: &mut Vec<Posn>, visited: &mut HashSet<Posn>) {
+    let (_, (direction, count)) = parse_move(line).unwrap();
     let delta = match direction {
-        "U" => Posn { x: 0, y: 1 },
-        "D" => Posn { x: 0, y: -1 },
-        "L" => Posn { x: -1, y: 0 },
-        "R" => Posn { x: 1, y: 0 },
-        _ => Posn { x: 0, y: 0 },
+        Direction::Up => Posn { x: 0, y: 1 },
+        Direction::Down => Posn { x: 0, y: -1 },
+        Direction::Left => Posn { x: -1, y: 0 },
+        Direction::Right => Posn { x: 1, y: 0 },
     };
     for _ in 0..count {
         // Move the very head of the rope first
-        rope[0].x += delta.x;
-        rope[0].y += delta.y;
+        rope[0] = rope[0] + delta;
         for idx in 1..rope.len() {
             let lead = rope[idx - 1];
             let follow = rope[idx];
@@ -65,32 +113,26 @@ fn execute_line(direction: &str, count: u32, rope: &mut Vec<Posn>, visited: &mut
 }
 
 pub fn part_one(input: &str) -> Option<u32> {
-    let start_pos = Posn { x: 0, y: 0 };
+    let start_pos = Posn::default();
     let mut locations = HashSet::<Posn>::new();
     let mut rope: Vec<Posn> = vec![start_pos; 2];
     locations.insert(rope.last().unwrap().clone());
 
     for line in input.lines() {
-        let mut words = line.split(" ");
-        let direction = words.next().unwrap();
-        let count = words.next().unwrap().parse::<u32>().unwrap();
-        execute_line(direction, count, &mut rope, &mut locations);
+        execute_line(line, &mut rope, &mut locations);
     }
 
     Some(locations.len() as u32)
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    let start_pos = Posn { x: 0, y: 0 };
+    let start_pos = Posn::default();
     let mut locations = HashSet::<Posn>::new();
     let mut rope: Vec<Posn> = vec![start_pos; 10];
     locations.insert(rope.last().unwrap().clone());
 
     for line in input.lines() {
-        let mut words = line.split(" ");
-        let direction = words.next().unwrap();
-        let count = words.next().unwrap().parse::<u32>().unwrap();
-        execute_line(direction, count, &mut rope, &mut locations);
+        execute_line(line, &mut rope, &mut locations);
     }
 
     Some(locations.len() as u32)
